a85d95be3f38a610d9d68a1311be2359ab1b4f62
anidakim <61341735+anida15@users.noreply.github.com>
2022-03-02 13:54:58 +0300

    update media urls
    the allow media to find a path in the system

--- hostelbookingapp/urls.py
+++ hostelbookingapp/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +11,5 @@ urlpatterns = [
     path('booking/', include('booking.urls')),
   
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
